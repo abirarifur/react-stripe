f7a18c5417fb7cc2836ba7323ac16ce9999cc2d8
abir arifur <abirarifur@gmail.com>
2022-04-16 23:31:18 +0600

    config stripe

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import './App.css';
 import StripeContainer from './component/StripeContainer';
 import spatula from './asstes/pro01.jpg'
 import { useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
 function App() {
-  const [showItem, setShowItem] = useState(false)
+  const [product] = useState({
+    name: "spatula",
+    price: 10.00
+  })
+  function handleToken(token, addresses) {
+    console.log(token, addresses);
+  }
   return (
     <div className="App">
       <h1>The Spatula Store</h1>
-      {showItem ? <StripeContainer /> : <><h3>$10.00</h3> <img src={spatula} alt="" /> <button onClick={() => setShowItem(true)}>Purchase</button> </>}
+      {product ? <><h3>$10.00</h3> <img src={spatula} alt="" /></>: ''}
+      <StripeCheckout stripeKey='pk_test_51KpDsTSABMecmyJbgLCohvtLvUCG834KZSASnRhbM5XBrkJepiiyiFAkG1GOgo9EPPZBj0fi2o0TOooPjJ3X9DCf00WoqC8Sfq'
+      token={handleToken}
+      />
     </div>
+    
   );
 }
 
